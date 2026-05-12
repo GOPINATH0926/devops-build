4c03588b22e17b4725088a7e0ca8869eaab1bd93
Ubuntu <ubuntu@ip-172-31-27-20.ap-south-1.compute.internal>
2026-05-12 06:21:38 +0000

    Fix Dockerfile to use pre-built React app

--- build.sh
+++ build.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-
-# Build script for Docker image
-set -e  # Stop on any error
+set -e
 
 BRANCH=$1
 DOCKERHUB_USER="gopinathsiva2605"
-IMAGE_TAG=$(git rev-parse --short HEAD)  # Use git commit hash as tag
+IMAGE_TAG=$(git rev-parse --short HEAD)
 
 echo "===== Building Docker Image ====="
 echo "Branch: $BRANCH"
 echo "Tag: $IMAGE_TAG"
 
-# Build the image
 docker build -t ${DOCKERHUB_USER}/dev:${IMAGE_TAG} .
 docker tag ${DOCKERHUB_USER}/dev:${IMAGE_TAG} ${DOCKERHUB_USER}/dev:latest
 
 echo "===== Build Complete ====="
-echo "Image: ${DOCKERHUB_USER}/dev:${IMAGE_TAG}"
